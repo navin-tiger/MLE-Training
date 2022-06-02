fa8a7296b9677dda57a8f16bd284bd80c5a16630
navin.kodam <navin.kodam@tigeranalytics.com>
2022-06-02 18:17:40 +0530

    error solved2

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -27,9 +27,11 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-housing = load_housing_data
+housing = load_housing_data()
 
 from sklearn.model_selection import train_test_split
+
+#print("Hello")
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
